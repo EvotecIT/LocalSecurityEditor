05317dd08beaf16d1cd54d753707f193fc669e43
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-08-10 07:33:29 +0200

    Refine pinned handle finalizer test

--- LocalSecurityEditor.Tests/Win32SecurityIdentifierTests.cs
+++ LocalSecurityEditor.Tests/Win32SecurityIdentifierTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using LocalSecurityEditor;
 using Xunit;
 
@@ -6,40 +8,30 @@ namespace LocalSecurityEditor.Tests;
 
 public class Win32SecurityIdentifierTests
 {
-#if NETFRAMEWORK
-    [Fact(Skip = "Pinned object count not available on .NET Framework")]
-    public void Finalizer_FreesPinnedHandle_NoDispose()
-    {
-    }
-#else
     [Fact]
     public void Finalizer_FreesPinnedHandle_NoDispose()
     {
-        if (!OperatingSystem.IsWindows())
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
             return;
         }
 
-        GC.Collect();
-        GC.WaitForPendingFinalizers();
-        GC.Collect();
-        long pinnedBefore = GC.GetGCMemoryInfo().PinnedObjectsCount;
-
         var sid = new Win32SecurityIdentifier("S-1-5-18");
-        GC.Collect();
-        GC.WaitForPendingFinalizers();
-        GC.Collect();
-        long pinnedDuring = GC.GetGCMemoryInfo().PinnedObjectsCount;
-        GC.KeepAlive(sid);
-        Assert.True(pinnedDuring > pinnedBefore);
+
+        var bufferField = typeof(Win32SecurityIdentifier).GetField("buffer", BindingFlags.NonPublic | BindingFlags.Instance)!;
+        var buffer = (byte[])bufferField.GetValue(sid)!;
+
+        var sidWeak = new WeakReference(sid);
+        var bufferWeak = new WeakReference(buffer);
 
         sid = null;
+        buffer = null;
+
         GC.Collect();
         GC.WaitForPendingFinalizers();
         GC.Collect();
 
-        long pinnedAfter = GC.GetGCMemoryInfo().PinnedObjectsCount;
-        Assert.Equal(pinnedBefore, pinnedAfter);
+        Assert.False(bufferWeak.IsAlive);
+        Assert.False(sidWeak.IsAlive);
     }
-#endif
 }
